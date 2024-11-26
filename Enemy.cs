5a453c25581f3ee698cd8b7760fb2cab094d400f
Sinow89 <christoffer.rozenbachs@gmail.com>
2024-11-26 15:55:10 +0100

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -5,4 +5,13 @@ public class Enemy : Character
     public Enemy(string name, int health) : base(name, health)
     {
     }
+
+    public void EnemeySwith(string input, Combat combat, Player player, Enemy enemy)
+    {
+        Console.WriteLine($"{input} says: Ready for battle!");
+        Console.WriteLine($"A wild {Name} with {Health} health!");
+        combat.Fight(player, enemy);
+        Console.WriteLine("");
+        Thread.Sleep(1000);
+    }
 }
